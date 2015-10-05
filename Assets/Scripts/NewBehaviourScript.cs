62cc9a5adaa5cc4d5f0cf50acbb9fe7894ad995c
Joshua Jones <the8bitbass@gmail.com>
2015-10-05 15:20:58 +1100

    standard update

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -9,7 +9,12 @@ public class NewBehaviourScript : MonoBehaviour
 		//doodle
         //fuckthishs=tasfnkl
         //doing thingamaboobs
-	
+        //dont cunt the dinosaur daddy
+        //fuck all tghis shot
+        //fuckyfuckmagoo    
+        //HA penis
+        //bigfloppydonkeydick
+        //but really penis
 	}
 
 	// Update is called once per frame
